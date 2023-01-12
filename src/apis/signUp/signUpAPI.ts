6543c866361d78eb5eb820869d164f886c34b825
young-st511 <asdk511@gmail.com>
2023-01-12 23:53:34 +0900

    ✨ feat(SignUp): SignUp API 함수 생성

---
+++ src/apis/signUp/signUpAPI.ts
@@ -0,0 +1,11 @@
+import { postRequest } from "apis/requests";
+import { SignUpApiUrl, SignUpRequest, SignUpResponse } from "./signUpAPI.type";
+
+export async function postSignUp(signUpFormData: SignUpRequest) {
+  const response = await postRequest<SignUpResponse, SignUpRequest>(
+    SignUpApiUrl.SIGN_UP,
+    signUpFormData
+  );
+
+  return response;
+}
