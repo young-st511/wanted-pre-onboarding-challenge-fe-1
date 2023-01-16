55817653a83be5431854867b9444daf93f97ebd5
young-st511 <asdk511@gmail.com>
2023-01-17 04:45:25 +0900

    ✨ feat(Todo): 할 일 생성 후 폼 reset

--- src/components/todos/TodoCreater.container.tsx
+++ src/components/todos/TodoCreater.container.tsx
@@ -4,14 +4,19 @@ import { useTodoCreate } from "../../hooks/queries/todos/useTodoMutation";
 import TodoEditerPresenter from "./TodoEditer.presenter";
 
 function TodoCreaterContainer() {
-  const { handleSubmit, formState, register } = useForm<UpdateTodoRequest>();
-  const { mutate } = useTodoCreate();
+  const { handleSubmit, formState, register, reset } =
+    useForm<UpdateTodoRequest>();
+  const { mutate, isSuccess } = useTodoCreate();
 
   const onSubmit: SubmitHandler<UpdateTodoRequest> = (
     data: UpdateTodoRequest
   ) => {
     mutate(data);
     console.log(data);
+
+    if (isSuccess) {
+      reset();
+    }
   };
 
   const props = { handleSubmit, formState, register, onSubmit };
