f0cf95cb038ff28971ed362131f0edaa81242f89
young-st511 <asdk511@gmail.com>
2023-01-16 20:19:50 +0900

    🚚 rename: StyledBox -> StyledRootBox

--- src/components/styles/StyledRootBox.tsx
+++ src/components/styles/StyledRootBox.tsx
@@ -1,15 +1,15 @@
 import styled from "styled-components";
 
-const StyledBox = styled.div`
+const StyledRootBox = styled.div`
   ${({ theme }) => theme.size.width};
   ${({ theme }) => theme.shadow.light};
-  
+
   box-sizing: border-box;
   margin: 10rem auto;
   padding: 3.2rem;
-  
+
   background-color: ${({ theme: { color } }) => color.white};
   border-radius: ${({ theme }) => theme.size.radius};
 `;
 
-export default StyledBox;
+export default StyledRootBox;
--- src/pages/SignUp/SignUp.style.tsx
+++ src/pages/SignUp/SignUp.style.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
-import StyledBox from "../../components/styles/StyledBox";
+import StyledRootBox from "../../components/styles/StyledRootBox";
 
-const Wrapper = styled(StyledBox)`
+const Wrapper = styled(StyledRootBox)`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -22,7 +22,7 @@ const Form = styled.form`
   gap: 2.4rem;
 
   margin: 2.4rem 0;
-  
+
   width: 80%;
   min-width: 310px;
 `;
@@ -30,16 +30,16 @@ const Form = styled.form`
 const Submit = styled.button`
   margin-top: 1.6rem;
   padding: 0.8rem 1.6rem;
-  
+
   background-color: ${({ theme: { color } }) => color.main};
   border-radius: 1rem;
-  
-  font-size:2rem;
+
+  font-size: 2rem;
   font-weight: 700;
   color: ${({ theme: { color } }) => color.white};
 
   &:hover {
-    ${({theme})=>theme.shadow.light};
+    ${({ theme }) => theme.shadow.light};
     scale: 1.01;
 
     transition: all 0.2s ease-in-out;
