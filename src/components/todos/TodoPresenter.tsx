1fc4dc0b5e7c1c9e53e41bbd9ba432c7b015cfb5
young-st511 <asdk511@gmail.com>
2023-01-17 04:34:14 +0900

    💄 style(Todo): Todo 카드 스타일링

--- src/components/todos/TodoPresenter.tsx
+++ src/components/todos/TodoPresenter.tsx
@@ -8,7 +8,13 @@ interface Props {
 function TodoPresenter({ todo }: Props) {
   return (
     <S.Wrapper>
-      <S.Title>{todo.title}</S.Title>
+      <S.HeaderBox>
+        <S.Title>{todo.title}</S.Title>
+        <S.ButtonBox>
+          <S.DeleteButton>삭제</S.DeleteButton>
+          <S.Button>수정</S.Button>
+        </S.ButtonBox>
+      </S.HeaderBox>
       <S.Contents>{todo.content}</S.Contents>
     </S.Wrapper>
   );
--- src/pages/Todos/Todos.style.tsx
+++ src/pages/Todos/Todos.style.tsx
@@ -7,6 +7,14 @@ export const Wrapper = styled(StyledRootBox)`
   padding: 2.4rem 2.4rem;
 `;
 
+export const HeaderBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const RightBox = styled.div``;
+
 export const List = styled.ul`
   display: flex;
   flex-direction: column;
@@ -24,12 +32,28 @@ export const Contents = styled.p`
   font-weight: 500;
 `;
 
-export const Button = styled(StyledButton)`
-  padding: 0.6rem 1.8rem;
-  margin-right: 1.2rem;
+export const ButtonBox = styled.div`
+  display: flex;
+`;
 
-  font-size: 1.6rem;
-  font-weight: 700;
+export const Button = styled(StyledButton)`
+  padding: 0.6rem 1.2rem;
 
   border-radius: 0.8rem;
+
+  color: ${({ theme: { color } }) => color.main};
+  background-color: ${({ theme: { color } }) => color.white};
+
+  font-size: 1.6rem;
+  font-weight: 600;
+
+  :hover {
+    background-color: ${({ theme: { color } }) => color.gray4};
+
+    transition: background-color 0.2s ease-in-out;
+  }
+`;
+
+export const DeleteButton = styled(Button)`
+  color: ${({ theme: { color } }) => color.sub};
 `;
