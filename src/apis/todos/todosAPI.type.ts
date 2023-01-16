e44a3772188c74484ac5024b5889e0317c92a9f8
young-st511 <asdk511@gmail.com>
2023-01-16 23:48:27 +0900

    🏷️ type: Todo API type 작성

---
+++ src/apis/todos/todosAPI.type.ts
@@ -0,0 +1,52 @@
+import { APIDomains } from "../apiDomains";
+
+export const TodosApiUrl = {
+  /** GET: 모든 TODO 받아오기 */
+  GET_TODOS: APIDomains.Todo,
+  /** GET: 개별 TODO id로 받아오기 `/todos/:id` */
+  GET_TODO_BY_ID: APIDomains.Todo + "/",
+  /** POST: TODO 만들기 */
+  POST_TODO: APIDomains.Todo,
+  /** PUT: TODO 수정하기  `/todos/:id` */
+  UPDATE_TODO: APIDomains.Todo + "/",
+  /** DELETE: TODO 삭제하기  `/todos/:id` */
+  DELETE_TODO: APIDomains.Todo + "/",
+} as const;
+
+interface Todo {
+  title: string;
+  content: string;
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface GetTodosResponse {
+  data: Todo[];
+}
+
+export interface GetTodoByIdResponse {
+  data: Todo;
+}
+
+export interface PostTodoRequest {
+  title: string;
+  content: string;
+}
+
+export interface PostTodoResponse {
+  data: Todo;
+}
+
+export interface UpdateTodoRequest {
+  title: string;
+  content: string;
+}
+
+export interface UpdateTodoResponse {
+  data: Todo;
+}
+
+export interface DeleteTodoResponse {
+  data: null;
+}
