534ff8eca396334b81c79eee1aabe9386676e1ba
Jung YoungHun <56586322+young-st511@users.noreply.github.com>
2023-01-19 04:00:38 +0900

    ✨ feat(Todo): Skeleton Props 수정

--- src/components/styles/SkeletonBox.tsx
+++ src/components/styles/SkeletonBox.tsx
@@ -2,15 +2,16 @@ import styled, { DefaultTheme } from "styled-components";
 
 interface SkeletonBoxProps {
   /** unit: rem */
-  width: number;
+  width?: number;
   /** unit: rem */
-  height: number;
-  color: Pick<DefaultTheme, "color">;
+  height?: number;
+  colorKey: keyof DefaultTheme["color"];
 }
 
 const SkeletonBox = styled.div<SkeletonBoxProps>`
-  width: ${({ width }) => width}rem;
-  height: ${({ height }) => height}rem;
+  width: ${({ width }) => (width ? width + "rem" : "unset")};
+  height: ${({ height }) => (height ? height + "rem" : "unset")};
+  color: ${({ theme, colorKey }) => theme.color[colorKey]};
 `;
 
 export default SkeletonBox;
--- src/features/todolist/components/TodoCardSkeleton.tsx
+++ src/features/todolist/components/TodoCardSkeleton.tsx
@@ -1,16 +1,21 @@
+import SkeletonBox from "../../../components/styles/SkeletonBox";
 import * as S from "./TodoCard/TodoCard.style";
 
 function TodoCardSkeleton() {
   return (
     <S.CardBox as={"article"}>
       <S.HeaderBox>
-        <S.Title>{todo.title}</S.Title>
+        <S.Title>
+          <SkeletonBox width={4} height={2} colorKey={"gray2"} />
+        </S.Title>
         <S.ButtonBox>
           <S.DeleteButton>삭제</S.DeleteButton>
           <S.EditButton>수정</S.EditButton>
         </S.ButtonBox>
       </S.HeaderBox>
-      <S.Contents>{todo.content}</S.Contents>
+      <S.Contents>
+        <SkeletonBox width={4} height={2} colorKey={"gray2"} />
+      </S.Contents>
     </S.CardBox>
   );
 }
