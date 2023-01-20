790e39389d4f2531bcb600db696aca3b97685147
young-st511 <asdk511@gmail.com>
2023-01-20 17:36:38 +0900

    🧑‍💻 ImpDX: open Chrome at Vite server startup

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 import dns from "dns";
 dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: "/",
-  },
+export default defineConfig(({ mode }) => {
+  if (mode === "development")
+    process.env.BROWSER = loadEnv(mode, process.cwd(), "").BROWSER;
+
+  return {
+    plugins: [react()],
+    server: {
+      open: true,
+    },
+  };
 });
